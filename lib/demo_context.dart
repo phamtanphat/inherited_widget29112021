25a0914cd40bcfe9673af9f20c09c69d5692a913
phamtanphat <phatdroid94@gmail.com>
2021-12-24 20:42:56 +0700

    lay data tu constructor

--- lib/demo_context.dart
+++ lib/demo_context.dart
@@ -11,7 +11,80 @@ class _DemoContextState extends State<DemoContext> {
       appBar: AppBar(
         title: Text("Demo build context"),
       ),
-      body: ,
+      body: Container(
+        child: Center(
+          child: OngBa(),
+        ),
+      ),
     );
   }
 }
+
+class OngBa extends StatefulWidget {
+
+  String text = "hello";
+
+  @override
+  _OngBaState createState() => _OngBaState();
+}
+
+class _OngBaState extends State<OngBa> {
+  @override
+  Widget build(BuildContext context) {
+    return Chame(widget.text);
+  }
+}
+
+class Chame extends StatelessWidget {
+
+  late String text;
+
+  Chame(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ConTrai(text),
+        ConGai(text)
+      ],
+    );
+  }
+}
+
+class ConTrai extends StatelessWidget {
+
+  late String text;
+
+  ConTrai(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(text),
+    );
+  }
+}
+
+class ConGai extends StatefulWidget {
+
+  late String text;
+
+  ConGai(this.text);
+
+  @override
+  _ConGaiState createState() => _ConGaiState();
+}
+
+class _ConGaiState extends State<ConGai> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(widget.text),
+    );
+  }
+}
+
+
+
+
